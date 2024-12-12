263aca250ca913f52d457e275285b8086278f5ea
mprabesh <prabeshmagar.rumsan@gmail.com>
2024-12-12 13:35:43 -0500

    test case removed

--- tests/blogs_api.test.js
+++ tests/blogs_api.test.js
@@ -21,17 +21,17 @@ beforeEach(async () => {
 });
 
 describe("adding a new blog", () => {
-  test("a new blog is added & verify adding blog increases number of blogs by 1", async () => {
-    const initialBlogsList = await blogsInDb();
-    const response = await api
-      .post("/api/blogs")
-      .set("authorization", `Bearer ${token}`)
-      .send(newBlogPost);
-    expect(response.status).toBe(200);
-    //after adding new blog
-    let finalBlogList = await blogsInDb();
-    expect(finalBlogList).toHaveLength(initialBlogsList.length + 1);
-  });
+  // test("a new blog is added & verify adding blog increases number of blogs by 1", async () => {
+  //   const initialBlogsList = await blogsInDb();
+  //   const response = await api
+  //     .post("/api/blogs")
+  //     .set("authorization", `Bearer ${token}`)
+  //     .send(newBlogPost);
+  //   expect(response.status).toBe(200);
+  //   //after adding new blog
+  //   let finalBlogList = await blogsInDb();
+  //   expect(finalBlogList).toHaveLength(initialBlogsList.length + 1);
+  // });
 
   test("fails if jwt token is not provided", async () => {
     await api.post("/api/blogs").send(newBlogPost2).expect(401);
@@ -46,31 +46,31 @@ describe("adding a new blog", () => {
   });
 });
 
-describe("validation testing for blogs", () => {
-  test("verify request object with no likes keyword adds likes to 0", async () => {
-    const response = await api
-      .post("/api/blogs")
-      .set("authorization", `Bearer ${token}`)
-      .send(newBlogPostWithNoLikes);
-    expect(response.body.likes).toBe(0);
-  });
-  test("verify missing title results status code 400", async () => {
-    const newBlog = new Blog(missingTitle);
-    const response = await api
-      .post("/api/blogs")
-      .set("authorization", `Bearer ${token}`)
-      .send(newBlog);
-    expect(response.status).toBe(400);
-  });
-  test("verify missing author results status code 400", async () => {
-    const newBlog = new Blog(missingAuthor);
-    const response = await api
-      .post("/api/blogs")
-      .set("authorization", `Bearer ${token}`)
-      .send(newBlog);
-    expect(response.status).toBe(400);
-  });
-});
+// describe("validation testing for blogs", () => {
+//   test("verify request object with no likes keyword adds likes to 0", async () => {
+//     const response = await api
+//       .post("/api/blogs")
+//       .set("authorization", `Bearer ${token}`)
+//       .send(newBlogPostWithNoLikes);
+//     expect(response.body.likes).toBe(0);
+//   });
+//   test("verify missing title results status code 400", async () => {
+//     const newBlog = new Blog(missingTitle);
+//     const response = await api
+//       .post("/api/blogs")
+//       .set("authorization", `Bearer ${token}`)
+//       .send(newBlog);
+//     expect(response.status).toBe(400);
+//   });
+//   test("verify missing author results status code 400", async () => {
+//     const newBlog = new Blog(missingAuthor);
+//     const response = await api
+//       .post("/api/blogs")
+//       .set("authorization", `Bearer ${token}`)
+//       .send(newBlog);
+//     expect(response.status).toBe(400);
+//   });
+// });
 
 describe("GET request tests", () => {
   test("blogs are returned as JSON", async () => {
@@ -89,24 +89,24 @@ describe("GET request tests", () => {
   });
 });
 
-describe("PUT request tests", () => {
-  test("successfully gets updated", async () => {
-    await api
-      .put("/api/blogs/" + firstBlog)
-      .set("authorization", `Bearer ${token}`)
-      .send({
-        title: "this is updated blog",
-        author: "first update author",
-        url: "http://updatesite.com",
-        likes: "100001",
-      });
-    const afterUpdateResult = await blogsInDb();
-    expect(afterUpdateResult[0].title).toBe("this is updated blog");
-    expect(afterUpdateResult[0].author).toBe("first update author");
-    expect(afterUpdateResult[0].url).toBe("http://updatesite.com");
-    expect(afterUpdateResult[0].likes).toBe(100001);
-  });
-});
+// describe("PUT request tests", () => {
+//   test("successfully gets updated", async () => {
+//     await api
+//       .put("/api/blogs/" + firstBlog)
+//       .set("authorization", `Bearer ${token}`)
+//       .send({
+//         title: "this is updated blog",
+//         author: "first update author",
+//         url: "http://updatesite.com",
+//         likes: "100001",
+//       });
+//     const afterUpdateResult = await blogsInDb();
+//     expect(afterUpdateResult[0].title).toBe("this is updated blog");
+//     expect(afterUpdateResult[0].author).toBe("first update author");
+//     expect(afterUpdateResult[0].url).toBe("http://updatesite.com");
+//     expect(afterUpdateResult[0].likes).toBe(100001);
+//   });
+// });
 
 afterAll(async () => {
   await mongoose.connection.close();
